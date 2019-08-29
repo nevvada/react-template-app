14ded8080cd7bf7fff9b7535652729ff30b829a6
nlemast <nlemaster47@gmail.com>
2019-08-29 16:30:00 -0400

    created base files: index.html, App component, and index.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <p>Hello, welcome to my template.</p>
+      </>
+    );
+  }
+}
+
+export default App;
